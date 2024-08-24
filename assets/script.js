c358638bfe3f65f94dd3bf76903f96356c6cc036
JSD <js.dohil@gmail.com>
2024-08-24 15:24:04 -0700

    added javascript file

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+var topics = ["HTML", "CSS", "JAVASCRIPT", "GIT"];
+// if(topic === 'html'){
+//     console.log("Lets Study Html");
+// }else if(topic === 'css'){
+//     console.log("Lets Study Css");
+// }else if(topic === 'javascript'){
+//     console.log("Lets Study javascript");
+// }else if(topic === 'git'){
+//     console.log("Lets Study Html");
+// }
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
